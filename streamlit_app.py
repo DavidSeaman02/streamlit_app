b88f7ae7e676d3b0ed2bc88fc65e72e8578990ca
DavidSeaman02 <44233991+DavidSeaman02@users.noreply.github.com>
2023-03-23 14:27:44 +0000

    Update streamlit_app.py
    Adding a text box for users to input a fruit they want to look for and hooking this up to the api call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to show api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
 streamlit.header('Fruityvice Fruit Advice!')
-
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Making the json format able to be used for a dataframe
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Putting the fruityvice data into a table
