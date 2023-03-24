1290b3770323a6cc8b407b12d461ba7e83cc22ef
DavidSeaman02 <44233991+DavidSeaman02@users.noreply.github.com>
2023-03-24 13:12:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-
+from urlib.error import URLError
 
 streamlit.title("My Parent's Healthy New Diner")
 
@@ -26,15 +26,28 @@ streamlit.dataframe(fruits_to_show)
 # New section to show api response
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+
+def get_fruityvice_data(get_fruit_choice):
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      return fruityvice_normalized
+  
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice):
+      streamlit.dataframe(back_from_function):
+
+ 
+except URLError as e:
+  streamlit.error()
+
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# Making the json format able to be used for a dataframe
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Putting the fruityvice data into a table
-streamlit.dataframe(fruityvice_normalized)
 
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
