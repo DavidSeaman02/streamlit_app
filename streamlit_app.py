60e99e36405f4965e0478742ef97d8b88e05055d
DavidSeaman02 <44233991+DavidSeaman02@users.noreply.github.com>
2023-03-24 14:57:04 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,4 +70,4 @@ def insert_row_snowflake(new_fruit):
            my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
            return "Thanks for adding " + new_fruit    
 
- insert_row_snowflake(newfruit)
+insert_row_snowflake(newfruit)
